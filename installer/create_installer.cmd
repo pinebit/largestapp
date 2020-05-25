faa4534d39bd0674dd67ccf5bed3ad2526876cd9
Andrei Smirnov <andrei.smirnov@toptal.com>
2020-05-25 09:52:19 +0300

    Updated graphics and installer script.

--- installer/create_installer.cmd
+++ installer/create_installer.cmd
@@ -1,2 +1,4 @@
+pushd packages\com.largestapp.desktop\data
 c:\qt\5.14.1\msvc2017_64\bin\windeployqt.exe --qmldir c:\GitHub\LargestApp\qml LargestApp.exe
+popd
 c:\qt\Tools\QtInstallerFramework\3.2\bin\binarycreator.exe -c config\config.xml -p packages LargestAppInstaller.exe
